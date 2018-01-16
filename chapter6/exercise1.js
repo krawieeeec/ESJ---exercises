88f3e93fa0850bef71b375908942d3196161f25d
Dawid <dk62344@st.amu.edu.pl>
2018-01-16 15:47:37 +0100

    create chapter6 without exercise two.

---
+++ chapter6/exercise1.js
@@ -0,0 +1,37 @@
+(function() {
+
+    function Vector(x, y) {
+        if(!(this instanceof Vector)) {
+            return new Vector();
+        }
+
+        this.x = x;
+        this.y = y;
+    }
+
+    Vector.prototype.plus = function(vector) {
+        
+        this.x += vector.x;
+        this.y += vector.y;
+    };
+    Vector.prototype.minus = function(vector) {
+        this.x -= vector.x;
+        this.y -= vector.y;
+    };
+    Object.defineProperty(Vector.prototype, "length", {
+        get: function() {
+            var x = Math.pow(this.x, 2),
+            y = Math.pow(this.y, 2);
+
+            return Math.sqrt(x + y);
+        }
+    });
+
+    var vector1 = new Vector(3,4);
+    var vector2 = new Vector(3,-40);
+    vector1.plus(vector2);
+    console.log(vector1);
+    vector1.minus(vector2);
+    console.log(vector1);
+    console.log(vector1.length);
+}())
