c84bf0749342abdf855c34644870fa51424c8857
Dawid <dk62344@st.amu.edu.pl>
2018-01-17 18:53:49 +0100

    create exercise from chapter8

---
+++ chapter8/exercise2.js
@@ -0,0 +1,43 @@
+(function() {
+
+    var box = {
+        locked: true,
+        unlock: function() { this.locked = false; },
+        lock: function() { this.locked = true;  },
+        _content: [],
+        get content() {
+          if (this.locked) throw new Error("Locked!");
+          return this._content;
+        }
+      };
+
+    function withBoxUnlocked(body) {
+        if(!box.locked) {
+            body();
+        } else if(box.locked) {
+            box.unlock();
+            body();
+            if(!box.locked) {
+                box.lock();
+            }
+        }
+    }
+       
+      try {
+        // withBoxUnlocked(function() {
+        //   throw new Error("Pirates on the horizon! Abort!");
+        // });
+        withBoxUnlocked(function() {
+            box.content.push("gold piece");
+          });
+          
+      } catch (e) {
+        console.log("Error raised:", e);
+      } finally {
+          if(!box.locked) {
+            box.lock();
+          }
+      }
+
+      console.log(box.locked);
+}())
