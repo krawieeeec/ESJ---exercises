209813e6b7e5e194e6d05bbc850a9a0f640e9d0d
Dawid <dk62344@st.amu.edu.pl>
2018-01-18 12:41:33 +0100

    create exercises from chapter9

---
+++ chapter9/exercise1.js
@@ -0,0 +1,46 @@
+(function() {
+
+    function verify(regexp, yes, no) {
+        // Ignore unfinished exercises
+        console.log(regexp.source);
+        if (regexp.source == "...") return;
+        yes.forEach(function(s) {
+          if (!regexp.test(s))
+            console.log("Failure to match '" + s + "'");
+        });
+        no.forEach(function(s) {
+          if (regexp.test(s))
+            console.log("Unexpected match for '" + s + "'");
+        });
+    }
+
+    verify(/(car|cat)/,
+        ["my car", "bad cats"],
+        ["camper", "high art"]);
+ 
+    verify(/(pop|prop)/,
+        ["pop culture", "mad props"],
+        ["plop"]);
+ 
+    verify(/(ferret|ferry|ferrari)/,
+        ["ferret", "ferry", "ferrari"],
+        ["ferrum", "transfer A"]);
+ 
+    verify(/(ious$)|(ious )/,
+        ["how delicious", "spacious room"],
+        ["ruinous", "consciousness"]);
+ 
+    verify(/\s[.,:;]/,
+        ["bad punctuation ."],
+        ["escape the dot"]);
+ 
+    verify(/\w{7}/,
+        ["hottentottententen"],
+        ["no", "hotten totten tenten"]);
+ 
+    verify(/^(\w+[^e]+|[^e]+\w+)$/, 
+        ["red platypus", "wobbling nest"],
+        ["earth bed", "learning ape"]);
+ 
+
+}())
---
+++ chapter9/exercise3.js
@@ -0,0 +1,15 @@
+(function() {
+
+    var numbers1 =  ["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4",
+    "1e+12"],
+    numbers2 = ["1a", "+-1", "1.2.3", "1+1", "1e4.5", ".5.", "1f5",
+    "."],
+    regEx = /^[+\-]?((\d+(\.\d*)?)|(\.\d+))(([eE][+\-]?)\d+)?$/;
+    
+    numbers1.forEach(function(element) {
+        if(!regEx.test(element)) {
+            console.log("Failed to match '" + element + "'");
+        }
+    })
+
+}())
