514a32995c1cdab0bf21def2ee7800e23fa6d854
Dawid <dk62344@st.amu.edu.pl>
2018-01-18 12:53:18 +0100

    delete validation

--- chapter2/exercise4.js
+++ chapter2/exercise4.js
@@ -3,15 +3,11 @@
     displayFlexChessBoard = function(widthChessBoard, heightChessBoard) {
         var lineOfChessBoard = "", subStringLineOfChessBoard = "";
 
-        if((typeof widthChessBoard == "undefined") || (typeof heightChessBoard == "undefined")){
-            return console.log("You don't pass needed arguments to fucntion.");
-        } else {
             for(var positionInString = 0; positionInString < widthChessBoard; positionInString++) {
                 if((positionInString % 2) == 0) {
                     lineOfChessBoard += " ";
                 } else {
                     lineOfChessBoard += "#";
-                }
             }
             
             for(var rowOfChessBoard = 0; rowOfChessBoard < heightChessBoard; rowOfChessBoard++){           
